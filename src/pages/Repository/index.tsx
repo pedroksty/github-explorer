ca7dd76dbf7061589b97a07603328883fca6bc71
pedroksty <pedrotrab2132@gmail.com>
2020-08-02 00:55:08 -0300

    Add styles to repository route

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { useRouteMatch } from 'react-router-dom'
+import { useRouteMatch, Link } from 'react-router-dom'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
-// import { Container } from './styles';
+import logoImg from '../../assets/logo.svg'
+import { Header, RepositoryInfo, Issues } from './styles'
 
 interface RepositoryParams {
   repository: string
@@ -10,7 +12,55 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>()
 
-  return <h1>Repository:{params.repository}</h1>
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="github explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/52813792?s=460&u=ee9c8be21c18177a6cf9b214124eb99f178be76b&v=4"
+            alt="pedroksty"
+          />
+          <div>
+            <strong>pedroksty/gobarber</strong>
+            <p>descricao do repositorio</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1080</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>89</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to={'dasd'}>
+          <div>
+            <strong>strongg</strong>
+            <p>pppdwadaw</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  )
 }
 
 export default Repository
