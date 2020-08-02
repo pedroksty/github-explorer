865ed7777a8bda15abf2f1010b988b8e5ea7e8dc
pedroksty <pedrotrab2132@gmail.com>
2020-08-01 23:53:36 -0300

    Add params on navigate in routes

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useRouteMatch } from 'react-router-dom'
 
 // import { Container } from './styles';
 
+interface RepositoryParams {
+  repository: string
+}
+
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>
+  const { params } = useRouteMatch<RepositoryParams>()
+
+  return <h1>Repository:{params.repository}</h1>
 }
 
 export default Repository
